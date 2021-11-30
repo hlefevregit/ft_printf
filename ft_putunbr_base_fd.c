7336ce8696434bae3382e2134865f9cd99eeec91
hlefevregit <93387947+hlefevregit@users.noreply.github.com>
2021-11-30 18:11:08 +0100

    Add files via upload

--- ft_putunbr_base_fd.c
+++ ft_putunbr_base_fd.c
@@ -1,62 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_base_fd.c                                :+:      :+:    :+:   */
+/*   ft_putunbr_base_fd.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlefevre <hlefevre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 11:15:57 by hlefevre          #+#    #+#             */
-/*   Updated: 2021/11/13 14:31:19 by hlefevre         ###   ########.fr       */
+/*   Updated: 2021/11/16 14:41:35 by hlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_check_base(char *base)
+void	ft_putunbr_base(unsigned long nbr, char *base)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	while (base[i])
+	if (nbr >= 16)
 	{
-		if (base[i] == '-' || base[i] == '+')
-			return (0);
-		if (base[i] < 32 || base[i] > 126)
-			return (0);
-		j = i + 1;
-		while (base[j])
-			if (base[i] == base[j++])
-				return (0);
-		i++;
+		ft_putunbr_base(nbr / 16, base);
+		ft_putchar_fd(base[nbr % 16], 1);
 	}
-	if (i <= 1)
-		return (0);
-	return (i);
-}
-
-static void	ft_putunbr_recursive(unsigned long nbr, char *base, int fd)
-{
-	int	base_size;
-
-	base_size = ft_strlen(base);
-	if (nbr > 0)
-	{
-		ft_putunbr_recursive(nbr / base_size, base, fd);
-		ft_putchar_fd(base[nbr % base_size], fd);
-	}
-}
-
-void	ft_putunbr_base_fd(unsigned long nbr, char *base, int fd)
-{
-	int	base_size;
-
-	base_size = ft_check_base(base);
-	if (base_size)
-	{
-		if (nbr == 0)
-			ft_putchar_fd(base[0], fd);
-		else
-			ft_putunbr_recursive(nbr, base, fd);
-	}
+	else
+		ft_putchar_fd(base[nbr], 1);
 }
